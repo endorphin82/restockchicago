5de14885302986d3fa5b09a2bb08b94824e7aa91
endorphin82 <endorphin82@gmail.com>
2020-02-26 16:58:49 +0200

    add footer markup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Footer from "./compopnents/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Footer/>
     </div>
   );
 }
