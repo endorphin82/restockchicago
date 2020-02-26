dfefb90b7a996213884f94ac68d223511c2c210a
endorphin82 <endorphin82@gmail.com>
2020-02-26 21:23:37 +0200

    Updates

--- precache-manifest.f7b862a98ea88bbcb12c7987a33a3fa3.js
+++ precache-manifest.f7b862a98ea88bbcb12c7987a33a3fa3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e2257e5cd2ff974fb0074a1cde109f1",
+    "revision": "6c249dc4854152977560b6b6b55df761",
     "url": "./index.html"
   },
   {
-    "revision": "a234eddbf3f043e393a9",
-    "url": "./static/css/main.31403ba6.chunk.css"
+    "revision": "4db9c9311834045d2933",
+    "url": "./static/css/main.c34068c8.chunk.css"
   },
   {
     "revision": "f7aa20d9bb4ef8200303",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.d8c98d39.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a234eddbf3f043e393a9",
-    "url": "./static/js/main.b4c60138.chunk.js"
+    "revision": "4db9c9311834045d2933",
+    "url": "./static/js/main.a5c5642d.chunk.js"
   },
   {
     "revision": "49c491ab7e435dcb9f29",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.98f889c76d85ae2b0e8b999215d33922.js"
+  "./precache-manifest.f7b862a98ea88bbcb12c7987a33a3fa3.js"
 );
 
 self.addEventListener('message', (event) => {
