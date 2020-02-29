a5780bd0c98c6dd87249f1c598bc93ddbac94471
endorphin82 <endorphin82@gmail.com>
2020-02-29 03:04:26 +0200

    animate search-box

---
+++ src/compopnents/header/nav-bar/menu-items/menu-item-jordan/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MenuItemJordan = () => {
+  return (
+    <div className="menu-item__jordan jordan-menu">
+      <div className="jordan-menu__right">
+
+      </div>
+    </div>
+  );
+};
+
+export default MenuItemJordan;
