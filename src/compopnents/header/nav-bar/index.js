5801e32ca0046bd035a13d317e502300edee31c3
endorphin82 <endorphin82@gmail.com>
2020-03-06 20:25:23 +0200

    mobile navbar icons align center

--- src/compopnents/header/nav-bar/index.js
+++ src/compopnents/header/nav-bar/index.js
@@ -17,15 +17,15 @@ const NavBar = ({ActiveMenuItem, itemName, isOpenBurger}) => {
   return (
     <div className={isOpenBurger ?"header__navbar navbar active": "header__navbar navbar"}>
       <ul className="navbar__menu-list">
-        <li className={itemName === 'jordan'? "active navbar__jordan" : "navbar__jordan"}>
+        <li className={itemName === 'jordan'? "active navbar__jordan navbar__icon" : "navbar__jordan navbar__icon"}>
           <a href="#" onClick={() => handleClick("jordan")}/>
           <MenuItemJordan/>
         </li>
-        <li className={itemName === 'nike' ? "active navbar__nike" : "navbar__nike"}>
+        <li className={itemName === 'nike' ? "active navbar__nike navbar__icon" : "navbar__nike navbar__icon"}>
           <a href="#" onClick={() => handleClick("nike")}/>
           <MenuItemNike/>
         </li>
-        <li className={itemName === 'adidas' ? "active navbar__adidas" : "navbar__adidas"}>
+        <li className={itemName === 'adidas' ? "active navbar__adidas navbar__icon" : "navbar__adidas navbar__icon"}>
           <a href="#" onClick={() => handleClick("adidas")}/>
           <MenuItemAdidas/>
         </li>
