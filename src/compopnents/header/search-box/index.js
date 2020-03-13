12e740fc34691103999515f385e8ec3be55afa77
endorphin82 <endorphin82@gmail.com>
2020-03-13 12:41:35 +0200

    SearchBox

--- src/compopnents/header/search-box/index.js
+++ src/compopnents/header/search-box/index.js
@@ -9,50 +9,43 @@ const SearchBox = () => {
 
     console.log("isOpen", isOpen);
     console.log("isBlur", isBlur);
-    // console.log("isOpenClickToggle", isOpenClickToggle);
 
     if (isOpen) {
-        // document.getElementsByClassName("input")[0].focus();
         inputRef.current.focus();
     }
 
-    const handleClickOpen = (e) => {
-        console.log("Open");
-
-        e.stopPropagation();
-
-        toggleOpen(true);
-    }
-    const handleClickClose = (e) => {
-        console.log("Close");
-
-        e.stopPropagation();
-        toggleOpen(false);
-    }
     const handleClickToggle = (e) => {
         console.log("Togle");
 
         e.stopPropagation();
 
-        if (!isBlur) {
-            toggleOpen(!isOpen)
-        } else if (isBlur && !isOpen){
+            if (!isBlur && !isOpen) {
             toggleOpen(true);
         }
+
         toggleBlur(false);
     }
 
     const handleBlur = (e) => {
         console.log("Blur");
         e.stopPropagation();
-        toggleOpen(false);
+        if (isOpen) {
+            console.log("if !Blur");
+            toggleBlur(true);
+            toggleOpen(false);
+            setTimeout(() =>
+                { toggleBlur(false)},
+
+                100)
+        }
         toggleBlur(true)
+
     }
 
     return (
         <div onClick={handleClickToggle} className={isOpen ? "search-wrapper open" : "search-wrapper"}>
             <div className={isOpen ? "search-box open" : "search-box"}>
-                <div onClick={handleClickClose} className={isOpen ? "close open" : "close"}>
+                <div className={isOpen ? "close open" : "close"}>
                     <div className={isOpen ? "close__icon open" : "close__icon"}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
                             <path fill="#b2bbc4" d="M37.304 11.282l1.414 1.414-26.022 26.02-1.414-1.413z"></path>
@@ -63,7 +56,6 @@ const SearchBox = () => {
                 <input className="input"
                        ref={inputRef}
                        onBlur={handleBlur}
-                       onClick={handleClickOpen}
                        type="text"
                        name="search" autoComplete="off"/>
             </div>
