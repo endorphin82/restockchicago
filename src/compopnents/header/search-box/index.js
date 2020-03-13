b571b283ed944af7c9be3f69e40a67b65ae0c9c6
endorphin82 <endorphin82@gmail.com>
2020-03-13 19:49:26 +0200

    search

--- src/compopnents/header/search-box/index.js
+++ src/compopnents/header/search-box/index.js
@@ -1,37 +1,40 @@
 import React, {useRef, useState} from 'react';
-import './styles.scss'
+import './styles.scss';
 
 const SearchBox = () => {
-    const [isOpen, toggleOpen] = useState(false)
-    const [isBlur, toggleBlur] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
+    const [isBlur, setIsBlur] = useState(false)
 
-    const inputRef = useRef();
+    // const inputRef = useRef();
 
     if (isOpen) {
-        inputRef.current.focus();
+        // inputRef.current.focus();
     }
 
     const handleClickToggle = (e) => {
         e.stopPropagation();
 
-            if (!isBlur && !isOpen) {
-            toggleOpen(true);
+        if (!isBlur && !isOpen) {
+            setIsOpen(true);
         }
 
-        toggleBlur(false);
+        setIsBlur(false);
     }
 
     const handleBlur = (e) => {
         e.stopPropagation();
+
         if (isOpen) {
-            toggleBlur(true);
-            toggleOpen(false);
-            setTimeout(() =>
-                { toggleBlur(false)},
+            setIsBlur(true);
+            setIsOpen(false);
+            setTimeout(() => {
+                    setIsBlur(false)
+                },
 
                 100)
         }
-        toggleBlur(true)
+
+        setIsBlur(true)
 
     }
 
@@ -41,13 +44,13 @@ const SearchBox = () => {
                 <div className={isOpen ? "close open" : "close"}>
                     <div className={isOpen ? "close__icon open" : "close__icon"}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                            <path fill="#b2bbc4" d="M37.304 11.282l1.414 1.414-26.022 26.02-1.414-1.413z"></path>
-                            <path fill="#b2bbc4" d="M12.696 11.282l26.022 26.02-1.414 1.415-26.022-26.02z"></path>
+                            <path fill="#ffffff" d="M37.304 11.282l1.414 1.414-26.022 26.02-1.414-1.413z"></path>
+                            <path fill="#ffffff" d="M12.696 11.282l26.022 26.02-1.414 1.415-26.022-26.02z"></path>
                         </svg>
                     </div>
                 </div>
                 <input className="input"
-                       ref={inputRef}
+                       // ref={inputRef}
                        onBlur={handleBlur}
                        type="text"
                        name="search" autoComplete="off"/>
