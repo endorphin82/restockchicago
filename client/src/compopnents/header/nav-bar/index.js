78bc124e2c5861e1ab91f5dcc2a377e8ce973ffd
endorphin82 <endorphin82@gmail.com>
2020-03-21 16:49:39 +0200

    zip backup navbar

--- client/src/compopnents/header/nav-bar/index.js
+++ client/src/compopnents/header/nav-bar/index.js
@@ -1,4 +1,4 @@
-import React  from "react"
+import React from "react"
 import { useQuery } from "@apollo/react-hooks"
 import { connect } from "react-redux"
 import { ActiveMenuItem } from "../../../AC"
