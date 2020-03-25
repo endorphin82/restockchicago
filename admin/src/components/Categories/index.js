3ae8c456529ce836269b5a3623f00e633b8704e4
endorphin82 <endorphin82@gmail.com>
2020-03-25 19:26:41 +0200

    categories table

--- admin/src/components/Categories/index.js
+++ admin/src/components/Categories/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Button, Tooltip } from "antd"
 import { PlusOutlined } from "@ant-design/icons"
+import CategoriesTable from "../CategoriesTable"
 
 const Categories = () => {
   return (
     <>
       <h1>Categories</h1>
+      <CategoriesTable/>
       <Tooltip title="Add Category">
         <Button type="dashed" shape="circle"
                 style={{ position: "fixed", bottom: "50px", right: "50px", color: "#08c" }}
