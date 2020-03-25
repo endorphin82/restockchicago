6d923d16fc79c8b2c3a46297e9b748c10fdaa2b4
endorphin82 <endorphin82@gmail.com>
2020-03-25 18:18:43 +0200

    add buttons and tooltips

--- admin/src/components/Categories/index.js
+++ admin/src/components/Categories/index.js
@@ -1,9 +1,18 @@
 import React from "react"
+import { Button, Tooltip } from "antd"
+import { PlusOutlined } from "@ant-design/icons"
 
 const Categories = () => {
   return (
     <>
       <h1>Categories</h1>
+      <Tooltip title="Add Category">
+        <Button type="dashed" shape="circle"
+                style={{ position: "fixed", bottom: "50px", right: "50px", color: "#08c" }}
+        ><PlusOutlined
+          style={{ fontSize: "22px", color: "#08c" }}
+        /></Button>
+      </Tooltip>
     </>
   )
 }
