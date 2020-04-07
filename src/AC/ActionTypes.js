250f5875a95da5c416b70dcaf22d7684ecf043cd
endorphin82 <endorphin82@gmail.com>
2020-04-07 16:06:42 +0300

    env

--- src/AC/ActionTypes.js
+++ src/AC/ActionTypes.js
@@ -2,4 +2,4 @@ export const TOGGLE_BURGER_MENU = "TOGGLE_BURGER_MENU";
 export const ACTIVE_MENU_ITEM = "ACTIVE_MENU_ITEM";
 export const TOGGLE_IS_MOBILE = "TOGGLE_IS_MOBILE";
 
-export const BASE_URL = 'http://localhost:3005/graphql'
+export const BASE_URL =  process.env.REACT_APP_BASE_URL
